0f09bb9970404a0927cd9c3c6223357c010a7d92
Leon Edlund <Leone.edlund@gmail.com>
2024-04-12 15:41:24 +0200

    [P1-32] Making the code easier

--- js/save-data.js
+++ js/save-data.js
@@ -1,30 +1,55 @@
 const storageLocationName = "location";
 const storageTypeName = "type";
+const pageId = document.querySelector("body").id 
 
-if(document.querySelector("body").id == "cities") {
-  console.log("hej");
-  const cities = document.querySelectorAll(".cities a");
+// if(document.querySelector("body").id == "cities") {
+//   console.log("hej");
+//   const cities = document.querySelectorAll(".cities a");
 
-  cities.forEach((element) => 
-    element.addEventListener("click", () => {
-      let location = element.id;
-      localStorage.removeItem(storageLocationName)
-      localStorage.setItem(storageLocationName, location);
-    })
-  );
-};
+//   cities.forEach((element) => 
+//     element.addEventListener("click", () => {
+//       let location = element.id;
+//       localStorage.removeItem(storageLocationName)
+//       localStorage.setItem(storageLocationName, location);
+//     })
+//   );
+// };
 
-if (document.querySelector("body").id == "category") {
-  console.log("hej då");
-  const categories = document.querySelectorAll(".categories a");
+// if (document.querySelector("body").id == "category") {
+//   console.log("hej då");
+//   const categories = document.querySelectorAll(".categories a");
 
-  categories.forEach((element) =>
-    element.addEventListener("click", () => {
-      let category = element.id;
-      localStorage.removeItem(storageTypeName)
-      localStorage.setItem(storageTypeName, category);
-    })
-  );
-};
+//   categories.forEach((element) =>
+//     element.addEventListener("click", () => {
+//       let category = element.id;
+//       localStorage.removeItem(storageTypeName)
+//       localStorage.setItem(storageTypeName, category);
+//     })
+//   );
+// };
+
+
+
+
+const choices = document.querySelectorAll(".choices a");
+
+choices.forEach((element) =>
+  element.addEventListener("click", () => {
+    switch (pageId) {
+      case "cities":
+        let location = element.id;
+        localStorage.removeItem(storageLocationName)
+        localStorage.setItem(storageLocationName, location);
+        break;
+      case "category":
+        let category = element.id;
+        localStorage.removeItem(storageTypeName)
+        localStorage.setItem(storageTypeName, category);
+        break;
+    }
+  })
+);
+
+
 
 
