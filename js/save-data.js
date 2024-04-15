01b86fc803df2e6ede6c3d1326e81bc80982e77b
Leon Edlund <Leone.edlund@gmail.com>
2024-04-15 11:56:35 +0200

    [P1-32] Code to save users location

--- js/save-data.js
+++ js/save-data.js
@@ -7,18 +7,44 @@ choices.forEach((element) =>
   element.addEventListener("click", () => {
     switch (pageId) {
       case "cities":
+        if (element.id == "my-position") {
+          userLocation();
+        };
         let location = element.id;
-        localStorage.removeItem(storageLocationName)
+        localStorage.removeItem(storageLocationName);
         localStorage.setItem(storageLocationName, location);
         break;
       case "category":
         let category = element.id;
-        localStorage.removeItem(storageTypeName)
+        localStorage.removeItem(storageTypeName);
         localStorage.setItem(storageTypeName, category);
         break;
     }
   })
 );
+
+function userLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  
+  function success(position) {
+    console.log(position);
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    localStorage.removeItem("latitude");
+    localStorage.setItem("latitude", lat);
+    localStorage.removeItem("longitude");
+    localStorage.setItem("longitude", long);
+    console.log(localStorage.getItem("longitude"));
+    console.log(localStorage.getItem("latitude"));
+    window.location.href = "categories.html";
+  }
+
+  function error(error) {
+    console.log(error);
+  }
+}
 
 
 
