73a2bbf589758515d08dbf8b3998673e188d2410
Leon Edlund <Leone.edlund@gmail.com>
2024-04-16 12:48:47 +0200

    [P1-35] save user location on load

--- js/save-data.js
+++ js/save-data.js
@@ -3,13 +3,13 @@ const storageTypeName = "type";
 const pageId = document.querySelector("body").id 
 const choices = document.querySelectorAll(".choices a");
 
+if (pageId == "cities") userLocation(); // get user location
+
+// save users choices
 choices.forEach((element) =>
   element.addEventListener("click", () => {
     switch (pageId) {
       case "cities":
-        if (element.id == "my-position") {
-          userLocation();
-        };
         let location = element.id;
         localStorage.removeItem(storageLocationName);
         localStorage.setItem(storageLocationName, location);
@@ -23,13 +23,13 @@ choices.forEach((element) =>
   })
 );
 
+// save users location
 function userLocation() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
   }
   
   function success(position) {
-    console.log(position);
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
     localStorage.removeItem("latitude");
@@ -38,7 +38,7 @@ function userLocation() {
     localStorage.setItem("longitude", long);
     console.log(localStorage.getItem("longitude"));
     console.log(localStorage.getItem("latitude"));
-    window.location.href = "categories.html";
+    //window.location.href = "categories.html";
   }
 
   function error(error) {
