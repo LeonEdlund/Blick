a2f34f6ab63f0ef286eed1eb0eb94ddb77278351
Alexander.urniaz <au222fw@student.lnu.se>
2024-04-16 12:47:05 +0200

    Började om

--- js/script.js
+++ js/script.js
@@ -1,72 +1,15 @@
-let dialog;
-let uDialog;
-let nyResa;
-let nyUtgift;
+let nyResaDialog;
+let utgiftDialog;
+let nyResaKnapp;
+let nyUtgiftKnapp;
 let minBudget;
 let utgifter=[];
-let input;
-let uInput;
+let nyResaInput;
+let nyUtgiftInput;
 
-
+//ALEX-initiering av programmet
 function init(){
-    dialog = document.querySelector("#dialog")
-    uDialog = document.querySelector("#uDialog")
-    input = document.querySelector("#minBudget")
-    uInput = document.querySelector("#utgift")
-    nyResa = document.querySelector("#nyResa")
-    nyResa.addEventListener("click",function(){ openDialog(dialog)})
-    nyUtgift = document.querySelector("#nyUtgift")
-    nyUtgift.addEventListener("click", function(){openDialog(uDialog)})
     
-    
-
-
 }
 window.addEventListener("load", init)
 
-function openDialog(id){
-    id.showModal()
-    let close = document.querySelectorAll(".close")
-    for(let a = 0; a < close.length; a++)
-    close[a].addEventListener("click",function(){ closeDialog(id)})
-    
-}
-
-
-function closeDialog(id){
-    id.close()
-    minBudget = parseInt(input.value)
-    let newValue = parseInt(uInput.value)
-    console.log(newValue)
-    utgifter.push(newValue)
-    setBudget()
-    setExpenses()
-
-}
-
-function setBudget(){
-    let av = document.querySelector("#av")
-    av.innerHTML = minBudget
-}
-
-function setExpenses(){
-let utgift = document.querySelector("#utgifter")
-    utgifter.innerHTML = ""
-    for(let b=0;b < utgifter.length; b++){
-    utgift.innerHTML += "<li class = 'utgift'>" +utgifter[b]+"</li>"
-    }
-    calculateRest()
-}
-
-function calculateRest(){
-    
-    let spent = 0;
-
-    for(let c = 0; c < utgifter.length; c++){
-        spent += utgifter[c]
-    }
-
-    let kvar = document.querySelector("#kvar")
-    kvar.innerHTML = minBudget -= spent
-
-}
