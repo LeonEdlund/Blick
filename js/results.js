9985413a4becb218884746c3149e24abb9e7ba2f
Leon Edlund <Leone.edlund@gmail.com>
2024-04-26 19:43:08 +0200

    Simplify results.js

--- js/results.js
+++ js/results.js
@@ -8,48 +8,48 @@ let resultsArray = []; // array with results from SMAPI
 let pageName;
 
 // variables for SMAPI URL
-const key = "KZmupnUS"; // API-Key
-let method = "method=getAll"; // vilken metod som ska användas
-let type; // Types for SMAPI
-let description = ""; // description
-let sortBy = ""; // variable for sorting SMAPI data
+const API_CONFIG = {
+  key: "KZmupnUS",
+  controller: "establishment",
+  method: "method=getAll",
+  types: "",
+  description: "",
+  //sortBy: ""
+}
 
 // HTML-Elements
-let main;
-let sort; // list for sorting
-let amountElem; // element for amounts of results
-let list // UL element for results
-let title; // Element for title on page
+let sort, list;
+
+// initializing 
+window.addEventListener("load", init);
+window.addEventListener("unload", saveScrollPosition);
 
 async function init() {
-  main = document.querySelector("main");
-  title = document.querySelector("#page-title");
   sort = document.querySelector("select");
-  amountElem = document.querySelector("#sort p")
   list = document.querySelector("#list-of-results");
+  sort.addEventListener("change", sortResults);
 
   getUserChoices()
-  sort.addEventListener("change", () => {
-    sessionStorage.clear();
-    sortResults();
-  });
-
-  // wait for data to load and scroll to last point 
+  changeTitle();
   await getData();
-  
   scrollToLastPosition();
 }
-window.addEventListener("load", init);
 
-// Leon - save scroll position
-window.addEventListener("unload", () => {
-  const scrollPosition = {
-    "scrollPosition": window.scrollY,
-    "fromPage": pageName,
-    "sortOption": sort.selectedIndex
+// Leon - Change title on page
+function changeTitle() {
+  const titleElem = document.querySelector("#page-title");
+
+  const categoryTitles = {
+    food: "Mat & Dryck",
+    nature: "Naturupplevelser",
+    culture: "Kultur",
+    activity: "Aktiviteter",
   };
-  sessionStorage.setItem("scrollPosition", JSON.stringify(scrollPosition));
-});
+
+  const title = categoryTitles[storedCategory];
+  pageName = title;
+  titleElem.textContent = title;
+}
 
 // Leon - get users choices from localStorage
 function getUserChoices() {
@@ -66,7 +66,7 @@ function getUserChoices() {
   // Set correct SMAPI url variable based on location
   switch (storedLocation) {
     case "my-position":
-      method = "method=getFromLatLng"
+      API_CONFIG.method = "method=getFromLatLng"
       chosenLocation = `lat=${usersLat}&lng=${usersLng}`;
       break;
     case "öland":
@@ -77,55 +77,48 @@ function getUserChoices() {
   }
 
   // Set correct SMAPI url variable based on category
-  let title = "";
   switch (storedCategory) {
     case "food":
-      type = "types=food"
-      title = "mat & dryck";
-      changeTitle(title);
+      API_CONFIG.types = "types=food"
       break;
     case "nature":
       // type = "types=activity"
-      description = "descriptions=älgpark,camping,naturreservat"
-      title = "naturupplevelser";
-      changeTitle(title);
+      API_CONFIG.description = "descriptions=älgpark,camping,naturreservat"
       break;
     case "culture":
-      type = "types=attraction"
-      title = "kultur";
-      changeTitle(title);
+      API_CONFIG.types = "types=attraction"
       break;
     case "activity":
-      type = "types=activity";
-      description = "descriptions=nöjespark,temapark,älgpark,djurpark,simhall,gokart,zipline,nöjescenter,paintballcenter,hälsocenter,golfbana,bowlinghall,klippklättring,skateboardpark"
-      title = "aktiviteter";
-      changeTitle(title);
+      API_CONFIG.types = "types=activity";
+      API_CONFIG.description = "descriptions=nöjespark,temapark,älgpark,djurpark,simhall,gokart,zipline,nöjescenter,paintballcenter,hälsocenter,golfbana,bowlinghall,klippklättring,skateboardpark"
+      break;
   }
 }
 
 // Leon - get data from SMAPI
 async function getData() {
-  try {
-    const response = await fetch(`https://smapi.lnu.se/api/?api_key=${key}&debug=true&controller=establishment&${method}&${type}&${chosenLocation}&${description}&${sortBy}`);
+  const { key, controller, method, types, description, sortBy } = API_CONFIG;
+  const baseURL = `https://smapi.lnu.se/api/?api_key=${key}`;
+  const URLParams = `&controller=${controller}&${method}&${types}&${chosenLocation}&${description}`;
+  const URL = baseURL + URLParams;
+  const response = await fetch(URL);
 
-    if (!response.ok) {
-      console.log(`Error status: ${response.status}`);
-    }
+  if (!response.ok) {
+    console.log(`Error status: ${response.status}`);
+  }
 
-    const data = await response.json();
-    if (data.header.status === `OK`) {
-      printResults(data.payload);
-    } else {
-      console.log(data.header);
-      errorMessage();
-    }
-  } catch (error) {
+  const data = await response.json();
+  if (data.header.status === `OK`) {
+    printResults(data.payload);
+  } else {
+    console.log(data.header);
     errorMessage();
   }
 }
 
 // Leon - Prints results from SMAPI in list
 function printResults(data) {
+  const amountElem = document.querySelector("#sort p");
   list.innerHTML = "";
 
   amountElem.innerText = `Antal resultat: ${data.length} st`;
@@ -133,14 +126,18 @@ function printResults(data) {
 
   resultsArray = [];
   data.forEach((result) => {
-
-    // let newLi = document.createElement("li");
-    // newLi.innerHTML = generateHTML(result);
-
     let newLi = generateHTML(result);
     resultsArray.push(newLi); // Save results in array
-
     fragment.appendChild(newLi);
+  });
+  list.appendChild(fragment);
+}
+
+function printSortedResults() {
+  list.innerHTML = "";
+  let fragment = document.createDocumentFragment();
+  resultsArray.forEach((result) => {
+    fragment.appendChild(result);
   });
   list.appendChild(fragment);
 }
@@ -151,81 +148,44 @@ function generateHTML(result) {
   let priceFrom = getPrice(result.price_range);
   let lat = result.lat;
   let lng = result.lng;
-  let distanceFromUser = calculateDistance(usersLat, usersLng, lat, lng);
+  let distance = calculateDistance(usersLat, usersLng, lat, lng);
 
   // create all elements
-  const newLi = document.createElement("li");
-  const newLink = document.createElement("a");
-  const resultInfoDiv = document.createElement("div");
-  const img = document.createElement("img");
-  const titleElem = document.createElement("h2");
-  const descriptionElem = document.createElement("p");
-  const distanceElem = document.createElement("p");
-  const extraInfoDiv = document.createElement("div");
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <a href="result.html?id=${result.id}" class="list-item">
+    <img src="temporary-img/Artboard 1.svg" alt="">
+    <div class="result-info">
+      <h2>${result.name}</h2>
+      <p>${result.description}</p>
+      ${usersLat && usersLng ? `<p>Avstånd: ${distance} Km</p>` : ""}
+    </div>
+    <div class="result-extra-info">
+      <div class="rating"><p>${score}/5</p></div>
+        <p class="price">Från: ${priceFrom} Kr</p>
+      </div>
+  </a>`;
 
-  // add info to link
-  newLink.href = `result.html?id=${result.id}`;
-  newLink.classList.add("list-item");
-  newLi.appendChild(newLink);
-
-  // add info to result-info-div
-  resultInfoDiv.classList.add("result-info");
-
-  // add info to img
-  img.src = "temporary-img/Artboard 1.svg";
-
-  // add info to titleElem
-  const title = document.createTextNode(`${result.name}`);
-  titleElem.appendChild(title);
-
-  // add info to descriptionElem
-  const description = document.createTextNode(`${result.description}`);
-  descriptionElem.appendChild(description);
-
-  // add info to distanceElem
-  if (usersLat && usersLng) {
-    const distanceInfo = document.createTextNode(`Avstånd: ${distanceFromUser} Km`)
-    distanceElem.appendChild(distanceInfo);
-  }
-
-  // add info to extraInfoDiv
-  const ratingDiv = document.createElement("div");
-  const rating = document.createElement("p");
-  const ratingInfo = document.createTextNode(`${score}/5`);
-  rating.appendChild(ratingInfo);
-
-  const price = document.createElement("p");
-  const priceInfo = document.createTextNode(`Från: ${priceFrom} Kr`);
-  price.classList.add("price");
-  price.appendChild(priceInfo);
-
-  extraInfoDiv.classList.add("result-extra-info")
-  ratingDiv.classList.add("rating");
-
-  ratingDiv.appendChild(rating);
-  extraInfoDiv.appendChild(ratingDiv);
-  extraInfoDiv.appendChild(price);
-  // add elements to resultInfoDiv
-  resultInfoDiv.appendChild(titleElem);
-  resultInfoDiv.appendChild(descriptionElem);
-  resultInfoDiv.appendChild(distanceElem);
-
-  // add elements to newLink
-  newLink.appendChild(img);
-  newLink.appendChild(resultInfoDiv);
-  newLink.appendChild(extraInfoDiv);
-
-  // add id
-  newLi.setAttribute("data-distance", distanceFromUser);
-
-  // print out elements on page
-  return newLi;
+  li.setAttribute("data-distance", distance);
+  li.setAttribute("data-price", priceFrom);
+  li.setAttribute("data-rating", result.rating);
+  return li;
 }
 
-// Leon - Change title on page
-function changeTitle(category) {
-  pageName = category;
-  title.innerText = category;
+// Leon - Sort results
+function sortResults() {
+  resultsArray = resultsArray.sort((a, b) => {
+    const [dataA, dataB] = [a.dataset, b.dataset];
+    switch (sort.value) {
+      case "priceASC":
+        return dataA.price - dataB.price;
+      case "ratingASC":
+        return dataB.rating - dataA.rating;
+      case "distanceASC":
+        return dataA.distance - dataB.distance;
+    }
+  });
+  printSortedResults();
 }
 
 // Leon - get first number of price_range
@@ -235,65 +195,6 @@ function getPrice(priceRange) {
     let price = priceRange.substring(0, index);
     return price;
   }
-}
-
-// Leon - Sort results
-function sortResults() {
-  switch (sort.value) {
-    case "priceASC":
-      sortBy = "sort_in=ASC&order_by=price_range";
-      getData();
-      break;
-    case "priceDESC":
-      sortBy = "sort_in=DESC&order_by=price_range";
-      getData();
-      break;
-    case "ratingASC":
-      sortBy = "sort_in=ASC&order_by=rating";
-      getData();
-      break;
-    case "ratingDESC":
-      sortBy = "sort_in=DESC&order_by=rating";
-      getData();
-      break;
-    case "distanceASC":
-      resultsArray.sort((a, b) => a.dataset.distance - b.dataset.distance);
-      printResultsByDistance();
-      break;
-    case "distanceDESC":
-      resultsArray.sort((a, b) => b.dataset.distance - a.dataset.distance);
-      printResultsByDistance();
-  }
-
-  function printResultsByDistance() {
-    list.innerHTML = "";
-    let fragment = document.createDocumentFragment();
-    resultsArray.forEach((result) => {
-      fragment.appendChild(result);
-    });
-    list.appendChild(fragment);
-  }
-}
-
-// Leon - Show error incase of smapi failure
-function errorMessage() {
-  main.innerHTML = "";
-  const newHeader = document.createElement("h1");
-  const newHeaderText = document.createTextNode("något gick fel!");
-  newHeader.appendChild(newHeaderText);
-
-  const newP = document.createElement("p");
-  const newPText = document.createTextNode("gå tillbaka till startsidan");
-  newP.appendChild(newPText);
-
-  const newA = document.createElement("a");
-  newA.href = "index.html";
-  const newAText = document.createTextNode("Gå tillbaka");
-  newA.appendChild(newAText);
-
-  main.appendChild(newHeader);
-  main.appendChild(newP);
-  main.appendChild(newA);
 }
 
 // Leon - calculate distance from user *CHAT-GPT HJÄLP*
@@ -317,6 +218,15 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
   return distanceRounded;
 }
 
+function saveScrollPosition() {
+  const scrollPosition = {
+    scrollPosition: window.scrollY,
+    fromPage: pageName,
+    sortOption: sort.selectedIndex
+  };
+  sessionStorage.setItem("scrollPosition", JSON.stringify(scrollPosition));
+}
+
 // Leon - scroll to last position and save sorting
 function scrollToLastPosition() {
   const savedPosition = JSON.parse(sessionStorage.getItem("scrollPosition"));
@@ -326,4 +236,13 @@ function scrollToLastPosition() {
     window.scrollTo(0, savedPosition.scrollPosition);
     sortResults();
   }
+}
+
+// Leon - Show error incase of smapi failure
+function errorMessage() {
+  const main = document.querySelector("main");
+  main.innerHTML = `
+    <h1>Något Gick Fel</h1>
+    <a href="index.html">Gå tillbaka till startsidan</a>
+  `;
 }
