3fdc9abe2e058d22f6559a7e675c5a5e71c143fd
Jesper Milton <jm224an@student.lnu.se>
2024-05-03 10:55:10 +0200

    [P1-50] Added array to search bar

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,28 +1,35 @@
 const key = "KZmupnUS";
+let dataArray = [];
 
 function init() {
     document.getElementById("searchbar").addEventListener("input", searchFunc);
+    getData();
 }
 window.addEventListener("load", init);
+
+async function getData() {
+    const response = await fetch(`https://smapi.lnu.se/api/?api_key=${key}&debug=true&controller=establishment&method=getall`);
+
+    if (!response.ok){
+        console.log("Fel!");
+        return;
+    }
+
+    const apiData = await response.json();
+    dataArray = apiData.payload;
+
+}
 
 async function searchFunc() {
     const searchTerm = document.getElementById("searchbar").value.toLowerCase();
 
-
-    const response = await fetch(`https://smapi.lnu.se/api/?api_key=${key}&debug=true&controller=establishment&method=getall`);
-
-    if (!response.ok){
-        console.log("Fel!")
-    }
-
-    const apiData = await response.json();
-    const data = apiData.payload;
+    console.log(dataArray);
 
     const resultsDiv = document.getElementById("results");
     let listItems = "";
 
-    for (let i = 0; i < data.length; i++) {
-        const item = data[i];
+    for (let i = 0; i < dataArray.length; i++) {
+        const item = dataArray[i];
 
         if (item.name && item.name.toLowerCase().includes(searchTerm)) {
             listItems += `<li>${item.name}</li>`;
