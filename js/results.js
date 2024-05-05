22571fb8a36d38f4c3255550c43a0c55350f9d6a
Leon Edlund <Leone.edlund@gmail.com>
2024-05-05 15:09:46 +0200

    [P1-49] Add animations for page load

--- js/results.js
+++ js/results.js
@@ -111,6 +111,7 @@ async function getData() {
 
   const data = await response.json();
   if (data.header.status === `OK`) {
+    DOM_ELEMENTS.list.classList.add("fade-in");
     printResults(data.payload);
   } else {
     console.log(data.header);
