f8ba0437aa2134d7f245c0cd061cf1a294140d31
Alexander.urniaz <au222fw@student.lnu.se>
2024-05-07 13:52:32 +0200

    Allmänt tillsnyggande av budget samt tillägg av viss funktionalitet rörande budgetens sparanade och processen för att sätta en budget

--- js/aBudgetScript.js
+++ js/aBudgetScript.js
@@ -6,19 +6,32 @@ let fromResultTwo;
 let names = []
 //ALEX-initiering av programmet
 function init() {
+    let getBudget = localStorage.getItem("minBudget")
+    minBudget = parseInt(getBudget)
+    console.log(minBudget)
+    if (minBudget == null || minBudget == "") {
+        newTripFunc()
+    } else (setBudget())
     getStorage();
     getSearchData()
     fromResult = sessionStorage.getItem("fromResult")
     fromResultFunc()
 
-    let newTripBtn = document.querySelector("#change-budget")
-    newTripBtn.addEventListener("click", newTripFunc)
+    let newTripBtn = document.querySelector("#remove-trip")
+    newTripBtn.addEventListener("click", function () {
+        let removeBtns = document.querySelectorAll(".remove")
+        for (let f = 0; f < removeBtns.length; f++) {
+            removeBtnFunc(true, removeBtns[f].parentElement)
+        }
+        listSpednings()
+        newTripFunc(false)
+    })
 
     let newSpendBtn = document.querySelector("#new-spend-btn")
-    newSpendBtn.addEventListener("click", function(){ newSpendFunc(false)})
+    newSpendBtn.addEventListener("click", function () { newSpendFunc(false) })
 
-    let removeTrip = document.querySelector("#remove-trip")
-    removeTrip.addEventListener("click", function(){location.reload()})
+    let removeTrip = document.querySelector("#change-budget")
+    removeTrip.addEventListener("click", function () { newTripFunc() })
 
 }
 window.addEventListener("load", init)
@@ -29,7 +42,6 @@ function newTripFunc(wrong) {
     newTripDialog.showModal()
 
     let input = document.querySelector("#money-to-spend")
-
     let save = document.querySelector("#save")
     save.addEventListener("click", function () { checkIfNumber(newTripDialog, input, true) })
 
@@ -42,7 +54,7 @@ function newTripFunc(wrong) {
 //ALEX - Kontrollerar ifall användaren skrivit in siffror i dialogen och kallar därefter på adekvat funktion 
 function checkIfNumber(close, input, newBudget) {
     close.close()
-    
+
     let number = parseInt(input.value)
 
     if (isNaN(number)) {
@@ -65,11 +77,13 @@ function checkIfNumber(close, input, newBudget) {
             for (let a = 0; a < options.length; a++) {
                 if (options[a].checked) {
                     category = options[a].getAttribute("id")
+                    options[a].checked = false;
                 }
             }
-            spendings.push(new Spending(name, number, category))
+            spendings.unshift(new Spending(name, number, category))
             listSpednings()
             nameElem.value = ""
+
         }
 
 
@@ -83,6 +97,7 @@ function checkIfNumber(close, input, newBudget) {
 }
 //ALEX - Sätter budgeten och räknar ut vad som är kvar av den
 function setBudget() {
+    localStorage.setItem("minBudget", minBudget)
     let budgetElemLeft = document.querySelector("#av")
     budgetElemLeft.innerHTML = "Av: " + minBudget;
     let budgetElem = document.querySelector("#kvar")
@@ -91,29 +106,35 @@ function setBudget() {
 //ALEX - Öppnar dialog för att användaren ska kunna fylla i en ny utgift
 function newSpendFunc(wrong) {
     let name = document.querySelector("#name")
-    name.addEventListener("keyup", function(){searchFunc(name.value)})
+    name.addEventListener("input", function () { searchFunc(name.value) })
     let newSpendDialog = document.querySelector("#new-spend-dialog")
     newSpendDialog.showModal()
     let exit = document.querySelector("#exit")
-    exit.addEventListener("click", function(){newSpendDialog.close()})
+    exit.addEventListener("click", function () {
+        newSpendDialog.close();
+        fromResult = "";
+    })
     let input = document.querySelector("#amount")
     input.value = ""
     let save = document.querySelector("#close")
     let cloneSave = save.cloneNode(true)
     save.parentElement.replaceChild(cloneSave, save)
     cloneSave.addEventListener("click", function () { checkIfNumber(newSpendDialog, input, false) })
-    if(wrong==false){
+    if (wrong == false) {
         name.value == ""
         input.style.backgroundColor = "";
-        }
+        return;
+    }
     if (wrong == true) {
         input.style.backgroundColor = "red";
+        return;
     }
-    if (wrong == fromResult){
-       let name = document.querySelector("#name")
+    if (wrong == fromResult) {
+        let name = document.querySelector("#name")
         let radio = document.querySelector(fromResultTwo)
         radio.checked = true;
-         name.value = fromResultOne
+        name.value = fromResultOne
+        return;
 
     }
 }
@@ -132,7 +153,7 @@ function listSpednings() {
         let c = spendings[b];
         console.log(b)
         console.log(c)
-        ul.innerHTML += "<li class='" + c.category + "'><div><h3>" + c.name + "</h3>" + c.price + " Kr"+ "</div>" +  "<button class='remove'>Ta bort</button></li>"
+        ul.innerHTML += "<li class='" + c.category + "'><div><h3>" + c.name + "</h3>" + c.price + " Kr" + "</div>" + "<button class='remove'>Ta bort</button></li>"
         amountSpent += c.price
     }
     removeBtnFunc()
@@ -166,10 +187,10 @@ function calculatePerCategory() {
                 break;
         }
     }
-    
+
     let general = document.querySelector("#general");
-    general.innerHTML = 
-    `<li>
+    general.innerHTML =
+        `<li>
         <div>
             <img src="img/culture.svg" alt=""></img>
             <p>Boende</p>
@@ -206,7 +227,7 @@ function calculatePerCategory() {
     </li>`
 
     let generalCat = document.querySelectorAll(".spent");
-    
+
     for (let e = 0; e < generalCat.length; e++) {
         if (generalCat[e].innerHTML == "0 Kr") {
             generalCat[e].parentElement.style.display = "none";
@@ -214,53 +235,53 @@ function calculatePerCategory() {
     };
 }
 
-function removeBtnFunc(remove, e){
+function removeBtnFunc(remove, e) {
     let removeBtns = document.querySelectorAll(".remove")
-    for(let f = 0; f < removeBtns.length; f++){
-        removeBtns[f].addEventListener("click", function(){removeBtnFunc(true, this.parentElement)})
+    for (let f = 0; f < removeBtns.length; f++) {
+        removeBtns[f].addEventListener("click", function () { removeBtnFunc(true, this.parentElement) })
     }
-    if(remove == true){
+    if (remove == true) {
         spendings.splice(e, 1)
         console.log(spendings);
         listSpednings();
     }
 }
 //hämmtar sessionStorage som sparas i result.html och startar sedan inläggningen av denna i budgeten
-function fromResultFunc(){
-    if(fromResult == null||fromResult==""){
+function fromResultFunc() {
+    if (fromResult == null || fromResult == "") {
         return;
     }
     let storageArray = fromResult.split("&")
     fromResultOne = storageArray[0]
-    fromResultTwo = "#"+storageArray[1]
+    fromResultTwo = "#" + storageArray[1]
     sessionStorage.setItem("fromResult", "")
     newSpendFunc(fromResult)
 
 }
 //sparar användarens val
-function setStorage(){
-let dataToSave = "/";
-for(let j = 0; j < spendings.length; j++){
-let name = spendings[j].name
-let price = spendings[j].price
-let category = spendings[j].category
-dataToSave+=name+"&"+price+"&"+category+"/"
-}
-localStorage.setItem("storedData", dataToSave) 
+function setStorage() {
+    let dataToSave = "/";
+    for (let j = 0; j < spendings.length; j++) {
+        let name = spendings[j].name
+        let price = spendings[j].price
+        let category = spendings[j].category
+        dataToSave += name + "&" + price + "&" + category + "/"
+    }
+    localStorage.setItem("storedData", dataToSave)
 }
 //hämtar tidigare val 
-function getStorage(){
+function getStorage() {
     let storedData = localStorage.getItem("storedData")
-    if(storedData == null){
+    if (storedData == null) {
         return;
     }
     let dataArray = storedData.split("/")
-    for(let l = 0; l < dataArray.length; l++){
-        if(dataArray[l].length > 0){
-        let dataArrayArray = dataArray[l].split("&")
-        spendings.push(new Spending(dataArrayArray[0], parseInt(dataArrayArray[1]), dataArrayArray[2]))
-        console.log(spendings)
-        listSpednings()
+    for (let l = 0; l < dataArray.length; l++) {
+        if (dataArray[l].length > 0) {
+            let dataArrayArray = dataArray[l].split("&")
+            spendings.unshift(new Spending(dataArrayArray[0], parseInt(dataArrayArray[1]), dataArrayArray[2]))
+            console.log(spendings)
+            listSpednings()
         }
     }
 }
@@ -270,33 +291,34 @@ async function getSearchData() {
     let responseTwo = await response.json()
     let data = responseTwo.payload
 
-    for(let u = 0; u < data.length; u++){
+    for (let u = 0; u < data.length; u++) {
         names.push(data[u].name)
     }
-  }
+}
 
-function searchFunc(input){
-    let search = document.querySelector("#results")
-    console.log(input)
-    if(input == ""){
+function searchFunc(input) {
+    let searchWord = input.toLowerCase()
+    let search = document.querySelector("#results-budget")
+    console.log(searchWord)
+    if (input == "") {
         search.innerHTML = "";
         return;
     }
     let spaces = 0;
     search.innerHTML = "";
-    for(let p = 0; p<names.length; p++){
-        
-        if(names[p].includes(input) == true){
+    for (let p = 0; p < names.length; p++) {
+        let lowerCaseName = names[p].toLowerCase()
+        if (lowerCaseName.includes(searchWord) == true) {
             search.innerHTML += "<li>" + names[p] + "</li>";
             spaces++
-            if(spaces == 5){
+            if (spaces == 5) {
                 break;
+            }
         }
     }
-    }
-    let searchResults = document.querySelectorAll("#results li")
-    for (let b = 0; b < searchResults.length; b++){
-        searchResults[b].addEventListener("click", function(){
+    let searchResults = document.querySelectorAll("#results-budget li")
+    for (let b = 0; b < searchResults.length; b++) {
+        searchResults[b].addEventListener("click", function () {
             document.querySelector("#name").value = searchResults[b].innerHTML
             search.innerHTML = ""
         })
